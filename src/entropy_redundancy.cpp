428a0be2d056bbb176ba6455cd7b48b89548ab17
Nguyễn Thị Tuyết <abcdetuyetnguyen@gmail.com>
2026-04-17 21:57:45 +0700

    Refactor entropy and redundancy calculation output

--- src/entropy_redundancy.cpp
+++ src/entropy_redundancy.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 using namespace std;
 
-// Q1: tính entropy
 double calculate_entropy(string s) {
     map<char, int> freq;
 
@@ -22,23 +21,20 @@ double calculate_entropy(string s) {
     return entropy;
 }
 
-// Q2: tính redundancy
 double calculate_redundancy(double entropy) {
-    double max_entropy = log2(256); // ASCII
+    double max_entropy = log2(256);
     return max_entropy - entropy;
 }
 
 int main() {
     string s;
-
-    cout << "Nhap chuoi: ";
-    cin >> s;
+    getline(cin, s);
 
     double entropy = calculate_entropy(s);
     double redundancy = calculate_redundancy(entropy);
 
-    cout << "Entropy: " << entropy << endl;
-    cout << "Redundancy: " << redundancy << endl;
+    cout << entropy << endl;
+    cout << redundancy;
 
     return 0;
 }
